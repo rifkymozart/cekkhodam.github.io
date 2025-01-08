736026e088dc7e93eff81ece465db3e906fa1d37
rifkymozart <rifkymozart2@gmail.com>
2025-01-08 14:41:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ document.getElementById('form-yatim').addEventListener('submit', function (e) {
   const submitButton = document.querySelector('#form-yatim button[type="submit"]');
 
   //const memberAzzahra = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih', 'wahyu', 'abdul', 'muid', 'anjas', 'catur', 'dedy', 'dedi', 'apriyan', 'dicki'];
+  const khodamPilihan = ['Superman', 'Batman', 'singa', 'kingkong', 'dr strange', 'kucing', 'spongebob']
   const khodam = ['anjing', 'babi', 'kucing', 'beruk', 'tapir', 'srengenge', 'tuyul', 'macan', 'singa', 'jokowi', 'cobra', 'ayam', 'sapi', 'wewe gombel', 'cicak', 'monyet', 'naga', 'beruang', 'doraemon', 'spongebob', 'patrick', 'marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'mimi peri', 'bidadari'];
   //const ciwi = ['zakia', 'imah', 'imalatul'];
   //const khodamCiwi = ['marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'peri', 'bidadari'];
 
+  const pilihanCowo = ['rifki', 'rifky', 'merong'];
   const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)].toUpperCase();
+  const randomKhodamPilihan = khodamPilihan[Math.floor(Math.random() * khodamPilihan.length)].toUpperCase();
   //const randomKhodamCiwi = khodamCiwi[Math.floor(Math.random() * khodamCiwi.length)].toUpperCase();
 
   if (nama === '') {
     hasil.innerHTML = '<span style="color: red;">Nama tidak boleh kosong!</span>';
+  } else if (pilihanCowo.some(kata => nama.includes(kata))) {
+    hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodamPilihan}</span>`;
+    disableForm(submitButton);
   } else {
     hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodam}</span>`;
     disableForm(submitButton);
