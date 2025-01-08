498e878a0031e269aba2966f66c63f9814038c3c
rifkymozart <rifkymozart2@gmail.com>
2025-01-08 13:43:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ document.getElementById('form-yatim').addEventListener('submit', function (e) {
 
   const nama = document.getElementById('nama').value.trim().toLowerCase();
   const hasil = document.getElementById('hasil');
+  const submitButton = document.querySelector('#form-yatim button[type="submit"]');
 
-  const yatim = ['dedy', 'dedi', 'catur'];
-  const yatimPasif = 'apriyan';
-  const memberWartak = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih'];
+  //const memberAzzahra = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih', 'wahyu', 'abdul', 'muid', 'anjas', 'catur', 'dedy', 'dedi', 'apriyan', 'dicki'];
+  const khodam = ['anjing', 'babi', 'kucing', 'beruk', 'tapir', 'srengenge', 'tuyul', 'macan', 'singa', 'jokowi', 'cobra', 'ayam', 'sapi', 'wewe gombel', 'cicak', 'monyet', 'naga', 'beruang', 'doraemon', 'spongebob', 'patrick', 'marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'mimi peri', 'bidadari'];
+  //const ciwi = ['zakia', 'imah', 'imalatul'];
+  //const khodamCiwi = ['marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'peri', 'bidadari'];
+
+  const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)].toUpperCase();
+  //const randomKhodamCiwi = khodamCiwi[Math.floor(Math.random() * khodamCiwi.length)].toUpperCase();
 
   if (nama === '') {
     hasil.innerHTML = '<span style="color: red;">Nama tidak boleh kosong!</span>';
-  } else if (nama.includes(yatimPasif)) {
-    hasil.innerHTML = '<span style="color: orange;">Kamu Yatim Pasif!</span>';
-  } else if (yatim.some(kata => nama.includes(kata))) {
-    hasil.innerHTML = '<span style="color: green;">Kamu Yatim!</span>';
-  } else if (memberWartak.some(kata => nama.includes(kata))) {
-    hasil.innerHTML = '<span style="color: blue;">Kamu Bukan Yatim!</span>';
   } else {
-    hasil.innerHTML = '<span style="color: red;">Daftar Member Wartak Dulu AJG!</span>';
+    hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodam}</span>`;
+    disableForm(submitButton);
   }
 });
+
+// Fungsi untuk menonaktifkan form setelah submit
+function disableForm(submitButton) {
+  submitButton.disabled = true; // Menonaktifkan tombol submit
+  document.getElementById('nama').disabled = true; // Menonaktifkan input nama
+}
