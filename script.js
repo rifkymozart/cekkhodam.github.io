7260ff013dfb25be2b3b5a154b7f56f97acbcb55
Rifki Robi Rifai <rifkymozart2@gmail.com>
2025-01-09 13:18:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,29 +2,49 @@ document.getElementById('form-yatim').addEventListener('submit', function (e) {
   e.preventDefault();
 
   const nama = document.getElementById('nama').value.trim().toLowerCase();
+  const namaInput = document.getElementById('nama').value.trim();
   const hasil = document.getElementById('hasil');
+  const loader = document.getElementById('loader');
   const submitButton = document.querySelector('#form-yatim button[type="submit"]');
-
-  //const memberAzzahra = ['dicky', 'subani', 'bani', 'rifky', 'rifki', 'iqro', 'ilham', 'fakih', 'wahyu', 'abdul', 'muid', 'anjas', 'catur', 'dedy', 'dedi', 'apriyan', 'dicki'];
-  const khodamPilihan = ['Superman', 'Batman', 'singa', 'kingkong', 'dr strange', 'kucing', 'spongebob']
-  const khodam = ['anjing', 'babi', 'kucing', 'beruk', 'tapir', 'srengenge', 'tuyul', 'macan', 'singa', 'jokowi', 'cobra', 'ayam', 'sapi', 'wewe gombel', 'cicak', 'monyet', 'naga', 'beruang', 'doraemon', 'spongebob', 'patrick', 'marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'mimi peri', 'bidadari', 'gatot kaca', 'petruk', 'semar', 'biri biri'];
-  //const ciwi = ['zakia', 'imah', 'imalatul'];
-  //const khodamCiwi = ['marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie', 'capung', 'peri', 'bidadari'];
-
-  const pilihanCowo = ['rifki', 'rifky', 'merong'];
-  const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)].toUpperCase();
-  const randomKhodamPilihan = khodamPilihan[Math.floor(Math.random() * khodamPilihan.length)].toUpperCase();
-  //const randomKhodamCiwi = khodamCiwi[Math.floor(Math.random() * khodamCiwi.length)].toUpperCase();
 
   if (nama === '') {
     hasil.innerHTML = '<span style="color: red;">Nama tidak boleh kosong!</span>';
-  } else if (pilihanCowo.some(kata => nama.includes(kata))) {
-    hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodamPilihan}</span>`;
-    disableForm(submitButton);
-  } else {
-    hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodam}</span>`;
-    disableForm(submitButton);
+    return;
   }
+
+  // Nonaktifkan tombol submit selama proses berjalan
+  submitButton.disabled = true;
+  loader.style.display = 'block';
+  hasil.innerHTML = `<span style="color: gray;">Sedang mengecek khodam <strong>${namaInput}</strong></span>`;
+
+  // Simulasikan proses mendapatkan respons (5 detik)
+  setTimeout(() => {
+    const khodam = [
+      'anjing', 'babi', 'kucing', 'beruk', 'tapir', 'srengenge', 'tuyul', 'macan', 'singa', 'jokowi',
+      'cobra', 'ayam', 'sapi', 'wewe gombel', 'cicak', 'monyet', 'naga', 'beruang', 'doraemon',
+      'spongebob', 'patrick', 'marmut', 'merak', 'angsa', 'kucing', 'hamster', 'bebek', 'barbie',
+      'capung', 'mimi peri', 'bidadari', 'ratu pantai selatan', 'ratu pantai bkt', 'radja jawa',
+      'raja bekasi', 'ratu pantai marunda', 'badut dufan'
+    ];
+    const khodamPilihan = ['Superman', 'Batman', 'singa', 'kingkong', 'dr strange', 'kucing', 'spongebob'];
+    const pilihanCowo = ['rifki', 'rifky', 'merong'];
+
+    // Pilih khodam secara acak
+    const randomKhodam = khodam[Math.floor(Math.random() * khodam.length)].toUpperCase();
+    const randomKhodamPilihan = khodamPilihan[Math.floor(Math.random() * khodamPilihan.length)].toUpperCase();
+
+    // Sembunyikan loader dan tampilkan hasil berdasarkan kondisi
+    loader.style.display = 'none';
+
+    if (pilihanCowo.some(kata => nama.includes(kata))) {
+      hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodamPilihan}</span>`;
+    } else {
+      hasil.innerHTML = `<span style="color: blue;">Khodam kamu adalah ${randomKhodam}</span>`;
+    }
+
+    // Nonaktifkan form setelah submit
+    disableForm(submitButton);
+  }, 5000); // 5000ms = 5 detik
 });
 
 // Fungsi untuk menonaktifkan form setelah submit
